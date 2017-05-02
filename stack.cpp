b6fc5e8cabb791b458b2d0f0e4c04c8ff7ccc49a
Alex Prut <1648497+alexprut@users.noreply.github.com>
2017-05-02 20:37:46 +0200

    Stack bug fix

--- stack.cpp
+++ stack.cpp
@@ -1,24 +1,33 @@
 class Stack {
 public:
+    Stack() {}
     int pop() {
         if (topIndex != -1) {
             topIndex--;
-            return data[topIndex+1];
+            return _data[topIndex + 1];
         }
 
         return -1;
     }
+
     void push(int n) {
-        data.push_back(n);
         topIndex++;
+        if (topIndex > ((int)_data.size() - 1)) {
+            _data.push_back(n);
+        } else {
+            _data[topIndex] = n;
+        }
     }
+
     int top() {
-        return data[topIndex];
+        return _data[topIndex];
     }
+
     bool isEmpty() {
         return (topIndex == -1);
     }
+
 private:
-    vector<int> data;
+    vector<int> _data;
     int topIndex = -1;
 };
